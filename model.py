7a85bf19f267423c01b4a5b91001f1064e5fa29d
Pranav Chinmay Aditya <154324689+Meleegod01@users.noreply.github.com>
2025-03-16 21:14:58 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+define DisasterResourcePredictor(nn.Module):
+    def __init__(self, input_size):
+        super(DisasterResourcePredictor, self).__init__()
+        self.fc1 = nn.Linear(input_size, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, 3)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.fc3(x))
+        return self.fc4(x)
